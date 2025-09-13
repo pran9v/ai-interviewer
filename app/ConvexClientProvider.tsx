2a1fbec2dfc4cca00b87e2396126c164401ef2b2
anotherproton <macbook@Macbooks-MacBook-Air.local>
2025-09-13 13:48:56 +0530

    fix: Move ConvexReactClient initialization inside component to resolve hydration error

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -5,9 +5,9 @@ import { ReactNode } from "react";
 import Provider from "./Provider";
 
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
+    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+    
     return <ConvexProvider client={convex}>
         <Provider>{children}</Provider>
     </ConvexProvider>;
